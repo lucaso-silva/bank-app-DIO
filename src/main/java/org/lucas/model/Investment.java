6148eb0ecab3170d5350a38def341e2f37879c45
Lucas Oliveira <luc.osilva@gmail.com>
2025-09-06 18:56:55 -0700

    Override toString method

--- src/main/java/org/lucas/model/Investment.java
+++ src/main/java/org/lucas/model/Investment.java
@@ -5,5 +5,8 @@ public record Investment(
         long tax,
         long initialFunds
 ) {
-
+    @Override
+    public String toString() {
+        return "Investment{" + "id=" + id + ", tax=" + tax + ", initialFunds=" + (initialFunds/100) + "," + (initialFunds%100) + '}';
+    }
 }
--- src/main/java/org/lucas/model/Wallet.java
+++ src/main/java/org/lucas/model/Wallet.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-@ToString
+
 public abstract class Wallet {
     @Getter
     private final BankService service;
@@ -46,5 +46,10 @@ public abstract class Wallet {
 
     public List<MoneyAudit> getFinancialTransactions(){
         return money.stream().flatMap(m -> m.getHistory().stream()).toList();
+    }
+
+    @Override
+    public String toString() {
+        return "Wallet{" + "service=" + service + ",money=$" + money.size() / 100 + "," + money.size() % 100 + "}";
     }
 }
